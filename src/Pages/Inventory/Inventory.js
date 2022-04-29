25b748f1ff4b1f5f47839423dbbf614351ccffad
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-04-29 22:47:19 +0600

    items done

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router';
 
 const Inventory = () => {
+
+    
     const navigate = useNavigate();
     const navToManageInventory = () => navigate('/manageInventory');
     return (
--- src/Pages/Item/Item.js
+++ src/Pages/Item/Item.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
 import './Item.css'
 
@@ -7,12 +8,12 @@ const Item = (props) => {
     const navigate = useNavigate();
 
     const handleUpdate = _id => {
-        navigate(`/inventory/:${_id}`)
+        navigate(`/inventory/${_id}`)
     }
 
     return (
-        <div className='col-12 col-lg-4 border border-dark height'>
-            <div className='d-flex justify-center'>
+        <div className='col-12 col-lg-4'>
+            {/* <div className='d-flex justify-center'>
                 <img className="w-100 mx-auto" src={image} alt="" />
             </div>
             <div className='py-lg-2'>
@@ -21,8 +22,24 @@ const Item = (props) => {
                 <p>${price}</p>
                 <p>{quantity}</p>
                 <p>{supplierName}</p>
-                <button onClick={() => handleUpdate(_id)} className='btn btn-primary w-100'>Update</button>
-            </div>
+                <button onClick={() => handleUpdate(_id)} className='btn btn-outline-dark w-100'>Update</button>
+
+            </div> */}
+            <Card className="position-relative card bg-dark text-white overflow-hidden">
+                <Card.Img className='card-img' src={image} alt="Card image" />
+                <Card.ImgOverlay className='dark-overlay'>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        {description}
+                    </Card.Text>
+                    <div className='extra-info'>
+                        <Card.Text>Price : ${price}</Card.Text>
+                        <Card.Text>Quantity : {quantity}</Card.Text>
+                        <Card.Text>Supplier Name : {supplierName}</Card.Text>
+                    </div>
+                </Card.ImgOverlay>
+            </Card>
+            <button onClick={() => handleUpdate(_id)} className='btn btn-dark w-100'>Update</button>
         </div>
     );
 };
