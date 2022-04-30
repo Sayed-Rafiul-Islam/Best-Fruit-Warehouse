9b83b7dc95712efccd27fd84f9ec29fc7578b631
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-05-01 05:30:35 +0600

    404 added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>not found</h1>
+        <div className='text-center text-success mt-4'>
+            <img className='w-25' src="https://i.ibb.co/YypN0yq/sad.png" alt="" />
+            <h1>404</h1>
+            <h4 className='faded-text'>page not found</h4>
+            <p>The page you are looking for does not exists or another error occurred <br />
+                <b>Go Back</b> or head over to <b>https://fruit-warhouse.web.app</b> to choose a new direction</p>
         </div>
     );
 };
