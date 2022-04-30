943817dd67d2bb326141f1d01f9343603e215e04
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-04-30 17:03:16 +0600

    update item res

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -74,13 +74,13 @@ const Inventory = () => {
     const navigate = useNavigate();
     const navToManageInventory = () => navigate('/manageInventory');
     return (
-        <div>
-            <h1>inventory</h1>
-
-            <div>
+        <div className='mt-5 pt-5 text-center'>
+            <h1 className='mt-lg-0 mt-5 pt-lg-0 pt-5'>INVENTORY</h1>
+            <p><i>Update Item</i></p>
+            <div className=''>
                 {
                     item?._id ?
-                        <Card style={{ width: '18rem' }}>
+                        <Card className='mx-auto' style={{ width: '18rem' }}>
                             <Card.Img variant="top" src={image} />
                             <Card.Body>
                                 <Card.Title>{name}</Card.Title>
@@ -97,29 +97,30 @@ const Inventory = () => {
                                 <ListGroupItem>Supplier Name : {supplierName}</ListGroupItem>
                             </ListGroup>
                             <Card.Body>
-                                <InputGroup className="mb-3">
-                                    <Form onSubmit={handleRestock} className='w-75'>
-                                        <Form.Group className="mb-3" controlId="formBasicNumber">
-                                            <Form.Control name='number' type="number" placeholder="Enter amount" required />
-                                        </Form.Group>
-                                        <Button className='w-100 mb-2' variant="success" type="submit">
-                                            Restock
-                                        </Button>
-                                    </Form>
-                                </InputGroup>
-                                <button style={{ display: del === 'disable' && "none" }} onClick={() => handleDelete(1)} className='btn btn-danger'>Delete</button>
+                                <div className='ms-5'>
+                                    <InputGroup className="">
+                                        <Form onSubmit={handleRestock} className='w-100'>
+                                            <Form.Group className="mb-3" controlId="formBasicNumber">
+                                                <Form.Control name='number' type="number" placeholder="Enter amount" required />
+                                            </Form.Group>
+                                            <Button className='w-100 mb-2' variant="success" type="submit">
+                                                Restock
+                                            </Button>
+                                        </Form>
+                                    </InputGroup>
+                                </div>
+                                <button style={{ display: del === 'disable' && "none" }} onClick={() => handleDelete(1)} className='btn btn-danger d-block w-100 mb-5'>Delete</button>
                                 <button onClick={handleUpdateItem} className='btn btn-info'>Update</button>
                             </Card.Body>
                         </Card>
                         :
                         <Spinner className="spinner-border mx-auto" role="status">
                         </Spinner>
-
                 }
 
 
             </div>
-            <button onClick={navToManageInventory} className='btn btn-dark'>Manage Inventory</button>
+            <button onClick={navToManageInventory} className='btn btn-outline-success my-5 w-50'>Manage Inventory</button>
         </div >
     );
 };
