02f2f2c985fe28acf2fd91127df58974a42b57b5
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-05-03 09:49:41 +0600

    responsive modified

--- src/App.js
+++ src/App.js
@@ -30,8 +30,8 @@ function App() {
         <Route path='/addInventoryItem' element={<AddInventoryItem></AddInventoryItem>}></Route>
         <Route path='/myItems' element={<MyItems></MyItems>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="register" element={<Register></Register>}></Route>
-        <Route path="blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <ToastContainer />
